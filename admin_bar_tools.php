7d93cf5f030c956d1b4ec62c2aef9f2f9508b1f9
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-10-20 01:40:38 +0900

    インデント修正

--- admin_bar_tools.php
+++ admin_bar_tools.php
@@ -1,16 +1,16 @@
 <?php
-/*
-Plugin Name: Admin Bar Tools
-Plugin URI: https://www.braveryk7.com/
-Description: Use the admin bar conveniently.
-Version: 0.1
-Author: Ken-chan
-Author URI: https://twitter.com/braveryk7
-*/
+    /*
+    Plugin Name: Admin Bar Tools
+    Plugin URI: https://www.braveryk7.com/
+    Description: Use the admin bar conveniently.
+    Version: 0.1
+    Author: Ken-chan
+    Author URI: https://twitter.com/braveryk7
+    */
 
-include_once(dirname(__FILE__).'/modules/abt_admin.php');
-include_once(dirname(__FILE__).'/modules/abt_db.php');
-include_once(dirname(__FILE__).'/modules/abt_set_menu.php');
+    include_once(dirname(__FILE__).'/modules/abt_admin.php');
+    include_once(dirname(__FILE__).'/modules/abt_db.php');
+    include_once(dirname(__FILE__).'/modules/abt_set_menu.php');
 
-register_activation_hook(__FILE__, 'abt_create_db');
-register_activation_hook(__FILE__, 'abt_default_insert_db');
+    register_activation_hook(__FILE__, 'abt_create_db');
+    register_activation_hook(__FILE__, 'abt_default_insert_db');
