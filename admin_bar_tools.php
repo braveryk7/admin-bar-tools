cc5fe0f26dc4f37a06dc68861677b9988eb68d71
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-10-20 01:24:28 +0900

    管理バービューロジックのモジュール化/モジュール読み込み

--- admin_bar_tools.php
+++ admin_bar_tools.php
@@ -10,41 +10,7 @@ Author URI: https://twitter.com/braveryk7
 
 include_once(dirname(__FILE__).'/modules/abt_admin.php');
 include_once(dirname(__FILE__).'/modules/abt_db.php');
+include_once(dirname(__FILE__).'/modules/abt_set_menu.php');
 
 register_activation_hook(__FILE__, 'abt_create_db');
 register_activation_hook(__FILE__, 'abt_default_insert_db');
-
-function abt_add_adminbar($wpAdminbar) {
-
-    $url = urlencode(get_pagenum_link(get_query_var('paged')));
-    $joinUrlLists = ['1', '2', '4', '5'];
-
-    $wpAdminbar->add_node([
-        'id' => 'abt',
-        'title' => 'Admin Bar Tools',
-        'meta' => [
-            'target' => 'abt'
-        ]
-    ]);
-
-    global $wpdb;
-    $tableName = $wpdb->prefix . 'abt';
-
-    $result = $wpdb->get_results("SELECT * FROM $tableName");
-
-    foreach($result as $key => $value) {
-        if($value->status === '1') {
-            $wpAdminbar->add_node([
-                'id' => $value->shortname,
-                'title' => $value->name,
-                'parent' => 'abt',
-                'href' => in_array($value->id, $joinUrlLists, true) ? $value->url . $url : $value->url,
-                'meta' => [
-                    'target' => '_blank'
-                ]
-            ]);
-        };
-    };
-}
-
-add_action('admin_bar_menu', 'abt_add_adminbar', 999);
