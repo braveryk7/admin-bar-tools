27003f633063f75805c7be6d66b72023c5733900
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-10-19 16:41:47 +0900

    インクルード、インストール時のフック、表示部作成

--- admin_bar_tools.php
+++ admin_bar_tools.php
@@ -7,3 +7,83 @@ Version: 0.1
 Author: Ken-chan
 Author URI: https://twitter.com/braveryk7
 */
+
+include_once(dirname(__FILE__).'/modules/abt_admin.php');
+include_once(dirname(__FILE__).'/modules/abt_db.php');
+
+register_activation_hook(__FILE__, 'abt_create_db');
+register_activation_hook(__FILE__, 'abt_default_insert_db');
+
+function abt_add_adminbar($wpAdminbar) {
+
+    $url = urlencode(get_pagenum_link(get_query_var('paged')));
+
+    $wpAdminbar->add_node([
+        'id' => 'abt',
+        'title' => 'Admin Bar Tools',
+        'meta' => [
+            'target' => 'abt'
+        ]
+    ]);
+
+    global $wpdb;
+    $tableName = $wpdb->prefix . 'abt';
+
+    $result = $wpdb->get_results("SELECT * FROM $tableName");
+
+    // foreach($result as $key => $value) {
+
+    // }
+
+    $wpAdminbar->add_node([
+        'id' => 'abtpsi',
+        'title' => 'PageSpeed Insights',
+        'parent' => 'abt',
+        'href' => $result[0]->url . $url,
+        'meta' => [
+            'target' => '_blank'
+        ]
+    ]);
+
+    $wpAdminbar->add_node([
+        'id' => 'abtlh',
+        'title' => 'Lighthouse',
+        'parent' => 'abt',
+        'href' => $result[1]->url . $url,
+        'meta' => [
+            'target' => '_blank'
+        ]
+    ]);
+
+    $wpAdminbar->add_node([
+        'id' => 'abtgsc',
+        'title' => 'Google Search Console',
+        'parent' => 'abt',
+        'href' => $result[2]->url,
+        'meta' => [
+            'target' => '_blank'
+        ]
+    ]);
+
+    $wpAdminbar->add_node([
+        'id' => 'abtgc',
+        'title' => 'Google Cache',
+        'parent' => 'abt',
+        'href' => $result[3]->url . $url,
+        'meta' => [
+            'target' => '_blank'
+        ]
+    ]);
+
+    $wpAdminbar->add_node([
+        'id' => 'abtgidx',
+        'title' => 'Google Index',
+        'parent' => 'abt',
+        'href' => $result[4]->url . $url,
+        'meta' => [
+            'target' => '_blank'
+        ]
+    ]);
+}
+
+add_action('admin_bar_menu', 'abt_add_adminbar', 999);
