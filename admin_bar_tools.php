89bac6f94e3a46e73e99a29d819e59d84dc96b8f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-10-19 16:28:32 +0900

    プラグインコメント記述

---
+++ admin_bar_tools.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Admin Bar Links
+Plugin URI: https://www.braveryk7.com/
+Description: Use the admin bar conveniently.
+Version: 0.1
+Author: Ken-chan
+Author URI: https://twitter.com/braveryk7
+*/
