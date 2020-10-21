e954ca677f5f734874dae41a8d926474a95bbfd4
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-10-21 17:47:51 +0900

    Hook移行

--- admin_bar_tools.php
+++ admin_bar_tools.php
@@ -16,3 +16,6 @@
     include_once(dirname(__FILE__).'/modules/abt_admin.php');
     include_once(dirname(__FILE__).'/modules/abt_db.php');
     include_once(dirname(__FILE__).'/modules/abt_set_menu.php');
+
+    register_activation_hook(__FILE__, 'abt_create_db');
+    register_activation_hook(__FILE__, 'abt_default_insert_db');
