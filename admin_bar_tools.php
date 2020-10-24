dbaf9653a79e1e2b86c6218308def743552a05aa
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-10-25 01:42:24 +0900

    abt_add_adminbar()移設

--- admin_bar_tools.php
+++ admin_bar_tools.php
@@ -15,7 +15,6 @@
 
     include_once(dirname(__FILE__).'/modules/abt_admin.php');
     include_once(dirname(__FILE__).'/modules/abt_db.php');
-    include_once(dirname(__FILE__).'/modules/abt_set_menu.php');
 
     register_activation_hook(__FILE__, 'abt_create_db');
     register_activation_hook(__FILE__, 'abt_default_insert_db');
@@ -23,3 +22,45 @@
     add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'add_settings_links');
 
     register_uninstall_hook(__FILE__, 'abt_delete_db');
+
+    function abt_add_adminbar($wpAdminbar) {
+
+        $url = urlencode(get_pagenum_link(get_query_var('paged')));
+        $joinUrlLists = ['1001', '1002', '2002', '2003', '3001', '3002'];
+    
+        $wpAdminbar->add_node([
+            'id' => 'abt',
+            'title' => __('Admin Bar Tools', 'admin-bar-tools'),
+            'meta' => [
+                'target' => 'abt'
+            ]
+        ]);
+    
+        global $wpdb;
+        $tableName = $wpdb->prefix . 'abt';
+    
+        $result = $wpdb->get_results("SELECT * FROM $tableName");
+    
+        foreach($result as $key => $value) {
+            if($value->status === '1') {
+                if(!is_admin() && $value->id === '3003') {
+                    $linkUrl = $value->url . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+                } elseif(!is_admin()) {
+                    $linkUrl = in_array($value->id, $joinUrlLists, true) ? $value->url . $url : $value->url;
+                } elseif(is_admin()) {
+                    $linkUrl = $value->adminurl;
+                };
+                $wpAdminbar->add_node([
+                    'id' => $value->shortname,
+                    'title' => __($value->name, 'admin-bar-tools'),
+                    'parent' => 'abt',
+                    'href' => $linkUrl,
+                    'meta' => [
+                        'target' => '_blank'
+                    ]
+                ]);
+            };
+        };
+    }
+    
+    add_action('admin_bar_menu', 'abt_add_adminbar', 999);
