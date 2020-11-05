7fbc588e34b814e47592ddfb49f6890eef428fbc
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-06 00:09:14 +0900

    readme.txt用翻訳配列記述

---
+++ lib/abt-strings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admin Bar Tools readme.txt translation file.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Admin Bar Tools
+ * @since 0.0.1
+ */
+
+$translation_strings = [
+	__( 'What is Admin Bar Tools?', 'admin-bar-tools' ),
+	__( 'This plugin allows you to add useful links to the admin bar.', 'admin-bar-tools' ),
+	__( 'In order to check PageSpeed ​​Insights, Twitter search, and Google index, you had to open each site and copy and paste the URL to confirm it until now.', 'admin-bar-tools' ),
+	__( 'But Admin Bar Tools allows you to do it in just one click.', 'admin-bar-tools' ),
+	__( 'This is very innovative.', 'admin-bar-tools' ),
+	__( 'You can access each tool from individual pages, articles, and management screens, which eliminates the troublesome operations.', 'admin-bar-tools' ),
+	__( 'We\'re glad that you can concentrate on writing.', 'admin-bar-tools' ),
+];
