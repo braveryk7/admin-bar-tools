76a58fca352eb7c7b25df179099dad818a2dae97
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-07 15:46:18 +0900

    多言語対応

--- modules/cancel-activate.php
+++ modules/cancel-activate.php
@@ -14,13 +14,13 @@
 function cancel_activate() {
 	?>
 <div class="error">
-	<p><?php esc_html_e( '[Plugin error] Admin Bar Tools has been stopped because the PHP version is old.' ); ?></p>
+	<p><?php esc_html_e( '[Plugin error] Admin Bar Tools has been stopped because the PHP version is old.', 'admin-bar-tools' ); ?></p>
 	<p>
-		<?php esc_html_e( 'Admin Bar Tools requires at least PHP 7.3.0 or later.' ); ?>
-		<?php esc_html_e( 'Please upgrade PHP.' ); ?>
+		<?php esc_html_e( 'Admin Bar Tools requires at least PHP 7.3.0 or later.', 'admin-bar-tools' ); ?>
+		<?php esc_html_e( 'Please upgrade PHP.', 'admin-bar-tools' ); ?>
 	</p>
 	<p>
-		<?php esc_html_e( 'Current PHP version:' ); ?>
+		<?php esc_html_e( 'Current PHP version:', 'admin-bar-tools' ); ?>
 		<?php echo PHP_VERSION; ?>
 	</p>
 </div>
