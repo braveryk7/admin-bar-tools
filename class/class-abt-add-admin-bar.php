39d776df495c5cf5906ba86f9bb5d49794081b77
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-10 18:10:56 +0900

    管理バーのビューロジックclass化

---
+++ class/class-abt-add-admin-bar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Returns data class
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Admin Bar Tools
+ * @since 1.0.6
+ */
+
+declare( strict_types = 1 );
+
+/**
+ * Add Admin Bar Tools to admin bar.
+ */
