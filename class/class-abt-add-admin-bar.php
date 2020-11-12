3ea46be75a97ab6926a48546f5d070a7fe8c598c
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-12 13:41:51 +0900

    管理バーへの追加をログイン状態時に限定

--- class/class-abt-add-admin-bar.php
+++ class/class-abt-add-admin-bar.php
@@ -27,40 +27,42 @@ class Abt_Add_Admin_Bar {
 		$url            = rawurlencode( get_pagenum_link( get_query_var( 'paged' ) ) );
 		$join_url_lists = [ '1001', '1002', '2002', '2003', '3001', '3002' ];
 
-		$wp_admin_bar->add_node(
-			[
-				'id'    => 'abt',
-				'title' => __( 'Admin Bar Tools', 'admin-bar-tools' ),
-				'meta'  => [
-					'target' => 'abt',
-				],
-			]
-		);
+		if ( true === is_user_logged_in() ) {
+			$wp_admin_bar->add_node(
+				[
+					'id'    => 'abt',
+					'title' => __( 'Admin Bar Tools', 'admin-bar-tools' ),
+					'meta'  => [
+						'target' => 'abt',
+					],
+				]
+			);
 
-		$db_call = new Abt_Connect_Database();
-		$result  = $db_call->return_table_data( Abt_Return_Data::TABLE_NAME );
+			$db_call = new Abt_Connect_Database();
+			$result  = $db_call->return_table_data( Abt_Return_Data::TABLE_NAME );
 
-		foreach ( $result as $key => $value ) {
-			if ( '1' === $value->status ) {
-				if ( ! is_admin() && '3003' === $value->id && isset( $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI'] ) ) {
-					$link_url = $value->url . sanitize_text_field( wp_unslash( $_SERVER['HTTP_HOST'] ) ) . sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) );
-				} elseif ( ! is_admin() ) {
-					$link_url = in_array( $value->id, $join_url_lists, true ) ? $value->url . $url : $value->url;
-				} elseif ( is_admin() ) {
-					$link_url = $value->adminurl;
+			foreach ( $result as $key => $value ) {
+				if ( '1' === $value->status ) {
+					if ( ! is_admin() && '3003' === $value->id && isset( $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI'] ) ) {
+						$link_url = $value->url . sanitize_text_field( wp_unslash( $_SERVER['HTTP_HOST'] ) ) . sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) );
+					} elseif ( ! is_admin() ) {
+						$link_url = in_array( $value->id, $join_url_lists, true ) ? $value->url . $url : $value->url;
+					} elseif ( is_admin() ) {
+						$link_url = $value->adminurl;
+					};
+					$wp_admin_bar->add_node(
+						[
+							'id'     => $value->shortname,
+							'title'  => $value->name,
+							'parent' => 'abt',
+							'href'   => $link_url,
+							'meta'   => [
+								'target' => '_blank',
+							],
+						]
+					);
 				};
-				$wp_admin_bar->add_node(
-					[
-						'id'     => $value->shortname,
-						'title'  => $value->name,
-						'parent' => 'abt',
-						'href'   => $link_url,
-						'meta'   => [
-							'target' => '_blank',
-						],
-					]
-				);
 			};
-		};
+		}
 	}
 }
