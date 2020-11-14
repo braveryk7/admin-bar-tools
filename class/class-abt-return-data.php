364091ae8a22673359d0efdddb578ffe7e5d98f6
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-14 13:41:14 +0900

    言語識別コメント付与

--- class/class-abt-return-data.php
+++ class/class-abt-return-data.php
@@ -23,163 +23,163 @@ class Abt_Return_Data {
 	const PSI_LOCALES = [
 		'ar'    => [
 			'id'   => 'ar',
-			'name' => 'العربية',
+			'name' => 'العربية', // Arabic.
 		],
 		'bg_BG' => [
 			'id'   => 'bg',
-			'name' => 'Български',
+			'name' => 'Български', // Bulgarian.
 		],
 		'ca'    => [
 			'id'   => 'ca',
-			'name' => 'Català',
+			'name' => 'Català', // Catalan.
 		],
 		'cs'    => [
 			'id'   => 'cs',
-			'name' => 'Čeština',
+			'name' => 'Čeština', // Czech.
 		],
 		'da_DK' => [
 			'id'   => 'da',
-			'name' => 'Dansk',
+			'name' => 'Dansk', // Danish.
 		],
 		'da_DE' => [
 			'id'   => 'de',
-			'name' => 'Deutsch',
+			'name' => 'Deutsch', // German.
 		],
 		'el'    => [
 			'id'   => 'el',
-			'name' => 'Ελληνικά',
+			'name' => 'Ελληνικά', // Greek.
 		],
 		'en_US' => [
 			'id'   => 'us',
-			'name' => 'English (United States)',
+			'name' => 'English (United States)', // English(US).
 		],
 		'en_GB' => [
 			'id'   => 'en-GB',
-			'name' => 'English (UK)',
+			'name' => 'English (UK)', // English(UK).
 		],
-		'es'    => [
-			'id'   => 'es_ES',
-			'name' => 'Español',
+		'es_ES' => [
+			'id'   => 'es',
+			'name' => 'Español', // Spanish.
 		],
 		'fi'    => [
 			'id'   => 'fi',
-			'name' => 'Suomi',
+			'name' => 'Suomi', // Finnish.
 		],
 		'tl'    => [
 			'id'   => 'fil',
-			'name' => 'Tagalog',
+			'name' => 'Tagalog', // Tagalog.
 		],
 		'fr_FR' => [
 			'id'   => 'fr',
-			'name' => 'Français',
+			'name' => 'Français', // French.
 		],
 		'hi_IN' => [
 			'id'   => 'hi',
-			'name' => 'हिन्दी',
+			'name' => 'हिन्दी', // Hindi.
 		],
 		'hr'    => [
 			'id'   => 'hr',
-			'name' => 'Hrvatski',
+			'name' => 'Hrvatski', // Croatian.
 		],
 		'hu_HU' => [
 			'id'   => 'hu',
-			'name' => 'Magyar',
+			'name' => 'Magyar', // Hungarian.
 		],
 		'id_ID' => [
 			'id'   => 'id',
-			'name' => 'Bahasa Indonesia',
+			'name' => 'Bahasa Indonesia', // Indonesian.
 		],
 		'it_IT' => [
 			'id'   => 'it',
-			'name' => 'Italiano',
+			'name' => 'Italiano', // Italian.
 		],
 		'he_IL' => [
 			'id'   => 'iw',
-			'name' => 'עִבְרִית',
+			'name' => 'עִבְרִית', // Hebrew.
 		],
 		'ja'    => [
 			'id'   => 'ja',
-			'name' => '日本語',
+			'name' => '日本語', // Japanese.
 		],
 		'ko_KR' => [
 			'id'   => 'ko',
-			'name' => '한국어',
+			'name' => '한국어', // Korean.
 		],
 		'lt_LT' => [
 			'id'   => 'lt',
-			'name' => 'Lietuvių kalba',
+			'name' => 'Lietuvių kalba', // Lithuanian.
 		],
 		'lv'    => [
 			'id'   => 'lv',
-			'name' => 'Latviešu valoda',
+			'name' => 'Latviešu valoda', // Latvian.
 		],
 		'nl_NL' => [
 			'id'   => 'nl',
-			'name' => 'Nederlands',
+			'name' => 'Nederlands', // Dutch.
 		],
 		'nb_NO' => [
 			'id'   => 'no',
-			'name' => 'Norsk bokmål',
+			'name' => 'Norsk bokmål', // Norwegian.
 		],
 		'pl_PL' => [
 			'id'   => 'pl',
-			'name' => 'Polski',
+			'name' => 'Polski', // Polish.
 		],
 		'pt_BR' => [
 			'id'   => 'pt-BR',
-			'name' => 'Português do Brasil',
+			'name' => 'Português do Brasil', // Portuguese(Brazil).
 		],
 		'pt_PT' => [
 			'id'   => 'pt-PT',
-			'name' => 'Português',
+			'name' => 'Português', // Portuguese.
 		],
 		'ro_RO' => [
 			'id'   => 'ro',
-			'name' => 'Română',
+			'name' => 'Română', // Romanian.
 		],
 		'ru_RU' => [
 			'id'   => 'ru',
-			'name' => 'Русский',
+			'name' => 'Русский', // Russian.
 		],
 		'sk_SK' => [
 			'id'   => 'sk',
-			'name' => 'Slovenčina',
+			'name' => 'Slovenčina', // Slovak.
 		],
 		'sl_SI' => [
 			'id'   => 'sl',
-			'name' => 'Slovenščina',
+			'name' => 'Slovenščina', // Slovenian.
 		],
 		'sr_RS' => [
 			'id'   => 'sr',
-			'name' => 'Српски језик',
+			'name' => 'Српски језик', // Serbian.
 		],
 		'sv_SE' => [
 			'id'   => 'sv',
-			'name' => 'Svenska',
+			'name' => 'Svenska', // Swedish.
 		],
 		'th'    => [
 			'id'   => 'th',
-			'name' => 'ไทย',
+			'name' => 'ไทย', // Thai.
 		],
 		'tr_TR' => [
 			'id'   => 'tr',
-			'name' => 'Türkçe',
+			'name' => 'Türkçe', // Turkish.
 		],
 		'uk'    => [
 			'id'   => 'uk',
-			'name' => 'Українська',
+			'name' => 'Українська', // Ukrainian.
 		],
 		'vi'    => [
 			'id'   => 'vi',
-			'name' => 'Tiếng Việt',
+			'name' => 'Tiếng Việt', // Vietnamese.
 		],
 		'zh_CN' => [
 			'id'   => 'zh-CN',
-			'name' => '简体中文',
+			'name' => '简体中文', // Simplified Chinese.
 		],
 		'zh_TW' => [
 			'id'   => 'zh-TW',
-			'name' => '繁體中文',
+			'name' => '繁體中文', // traditional Chinese.
 		],
 	];
 
