c8fb9ca4182f66cc5792363faff5904c56422c45
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-09-29 18:11:14 +0900

    #18 fix call component
    add useContext, useState
    use custome hooks -> useGetApi
    render component after api connect
    add Items component
    add Radio component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,53 @@
 import './scss/index.scss';
 
+import { createContext, Dispatch, SetStateAction, useState } from 'react';
+
 import { render } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
-import { Checkbox } from './component/checkbox';
+import { Radio } from './component/molecules/Radio';
+import { Checkbox } from './component/molecules/checkbox';
+import { Items } from './component/organisms/Items';
+import { useGetApi } from './hooks/useGetApi';
+import { apiType } from './types/apiType';
+
+export const apiContext = createContext(
+	{} as {
+		apiData: apiType;
+		setApiData: Dispatch< SetStateAction< apiType > >;
+	}
+);
 
 const AdminPage = () => {
+	const [ apiData, setApiData ] = useState( {} );
+
+	useGetApi( setApiData );
+
 	return (
 		<div id="wrap">
 			<h1>{ __( 'Admin Bar Tools Settings', 'admin-bar-tools' ) }</h1>
-			<Checkbox />
+			{ Object.keys( apiData ).length && (
+				<apiContext.Provider value={ { apiData, setApiData } }>
+					<Items
+						title={ __(
+							'Please select the items you want to display.',
+							'admin-bar-tools'
+						) }
+						classValue="select-display"
+					>
+						<Checkbox itemKey="abt_status" />
+					</Items>
+					<Items
+						title={ __(
+							'Choose how you want to register with Google SearchConsole.',
+							'admin-bar-tools'
+						) }
+						classValue="select-radio"
+					>
+						<Radio itemKey="abt_sc" />
+					</Items>
+				</apiContext.Provider>
+			) }
 		</div>
 	);
 };
