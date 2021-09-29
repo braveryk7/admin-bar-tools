ab8198d7d4c66026dd8cefae0c97e172d733f27f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-09-29 17:35:36 +0900

    #18 add useGetApi
    this hooks is get WordPress settings API

---
+++ src/hooks/useGetApi.ts
@@ -0,0 +1,20 @@
+import { Dispatch, SetStateAction, useEffect } from 'react';
+
+// @ts-ignore
+import api from '@wordpress/api'; // eslint-disable-line
+import { apiType, WPApiType } from '../types/apiType';
+import { ItemType } from '../types/checkboxType';
+
+export const useGetApi = (
+	stateFunc: Dispatch< SetStateAction< apiType > >
+) => {
+	useEffect( () => {
+		api.loadPromise.then( () => {
+			const model = new api.models.Settings();
+
+			model.fetch().then( ( res: WPApiType< ItemType > ) => {
+				stateFunc( res );
+			} );
+		} );
+	}, [ stateFunc ] );
+};
