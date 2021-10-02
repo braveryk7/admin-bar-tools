e1aa25eab089393b280657d6a168d98ce23f4ea2
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-10-02 15:46:39 +0900

    #18 add Snackbar component logic, useEffect flag

--- src/hooks/useSetApi.ts
+++ src/hooks/useSetApi.ts
@@ -1,5 +1,10 @@
+import { useContext, useEffect, useRef } from 'react';
+
 // @ts-ignore
 import api from '@wordpress/api'; // eslint-disable-line
+import { __ } from '@wordpress/i18n';
+
+import { apiContext } from '..';
 import { WPApiType } from '../types/apiType';
 import { ItemsWrapperType, ItemType } from '../types/checkboxType';
 
@@ -7,17 +12,39 @@ export const useSetApi = (
 	itemKey: string,
 	value: ItemsWrapperType< ItemType > | number
 ) => {
-	api.loadPromise.then( () => {
-		const model = new api.models.Settings( {
-			[ itemKey ]: value,
-		} );
-		const save = model.save();
+	const { setNoticeStatus, setNoticeValue, setNoticeMessage } = useContext(
+		apiContext
+	);
 
-		save.success( ( res: WPApiType< ItemType >, status: string ) => {
-			return [ res, status ];
-		} );
-		save.error( ( res: WPApiType< ItemType >, status: string ) => {
-			return [ res, status ];
-		} );
-	} );
+	const isFirstRender = useRef( true );
+
+	useEffect( () => {
+		if ( isFirstRender.current ) {
+			isFirstRender.current = false;
+		} else {
+			api.loadPromise.then( () => {
+				const model = new api.models.Settings( {
+					[ itemKey ]: value,
+				} );
+				const save = model.save();
+
+				setNoticeStatus( false );
+
+				save.success(
+					( res: WPApiType< ItemType >, status: string ) => {
+						setNoticeStatus( true );
+						setNoticeValue( 'abt_success' );
+						setNoticeMessage( __( 'Success.', 'admin-bar-tools' ) );
+						return [ res, status ];
+					}
+				);
+				save.error( ( res: WPApiType< ItemType >, status: string ) => {
+					setNoticeStatus( true );
+					setNoticeValue( 'abt_error' );
+					setNoticeMessage( __( 'Error.', 'admin-bar-tools' ) );
+					return [ res, status ];
+				} );
+			} );
+		}
+	}, [ itemKey, value ] );
 };
