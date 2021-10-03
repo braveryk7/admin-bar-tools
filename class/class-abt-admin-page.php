542a054d9af3a877a8e4fc0e002d47fbf29f4ea1
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-10-03 21:39:09 +0900

    #25 fix settings page path

--- class/class-abt-admin-page.php
+++ class/class-abt-admin-page.php
@@ -49,7 +49,7 @@ class Abt_Admin_Page {
 	 * @param array|string $links plugin page setting links.
 	 */
 	public function add_settings_links( array $links ): array {
-		$add_link = '<a href="options-general.php?page=admin-bar-tools-settings">' . __( 'Settings', 'admin-bar-tools' ) . '</a>';
+		$add_link = '<a href="options-general.php?page=admin-bar-tools">' . __( 'Settings', 'admin-bar-tools' ) . '</a>';
 		array_unshift( $links, $add_link );
 		return $links;
 	}
