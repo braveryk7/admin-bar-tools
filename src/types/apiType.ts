1d26a13a76cb0f6a306267d8d1fb794fb041b3ba
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-02 16:34:22 +0900

    refs #43 add abt_locale

--- src/types/apiType.ts
+++ src/types/apiType.ts
@@ -1,6 +1,7 @@
 import { ItemsWrapperType, ItemType } from './CheckboxType';
 
 export type apiType = {
+	abt_locale?: string; // eslint-disable-line
 	abt_sc?: number; // eslint-disable-line
 	abt_status?: ItemsWrapperType< ItemType >; // eslint-disable-line
 };
