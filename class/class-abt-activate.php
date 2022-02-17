90d869675f26b9f24ae0fffc6c24af373b3bc83b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-17 14:24:15 +0900

    refs #83 add class base

---
+++ class/class-abt-activate.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin activate.
+ *
+ * @author     Ken-chan
+ * @package    WordPress
+ * @subpackage Admin Bar Tools
+ * @since      1.5.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Activate process.
+ */
+class Abt_Activate extends Sct_Base {
+
+}
