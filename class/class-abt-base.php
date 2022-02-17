59a2a0263a133bddfd4482ad1e88eeb0fe8aee9b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-17 13:15:31 +0900

    refs #76 TABLE_NAME

--- class/class-abt-base.php
+++ class/class-abt-base.php
@@ -22,6 +22,7 @@ class Abt_Base {
 	protected const PLUGIN_SLUG = 'admin-bar-tools';
 	protected const PLUGIN_NAME = 'Admin Bar Tools';
 	protected const PLUGIN_FILE = self::PLUGIN_SLUG . '.php';
+	protected const TABLE_NAME  = self::PREFIX;
 
 	/**
 	 * Return add prefix.
