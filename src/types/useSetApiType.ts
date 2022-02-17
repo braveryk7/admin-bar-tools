2647ff27ea6764b2d5f6d6b408830aef2311b052
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-18 00:23:45 +0900

    refs #86 delete

--- src/types/useSetApiType.ts
+++
@@ -1,8 +0,0 @@
-import { ItemsWrapperType, ItemType } from './CheckboxType';
-
-export type useSetApiType = {
-	(
-		itemKey: string,
-		value: ItemsWrapperType< ItemType > | string | number
-	): void;
-};
