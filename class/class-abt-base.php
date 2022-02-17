02061c2e53ebebe24a851612c4d7855a25c209d1
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-17 12:46:23 +0900

    refs #76 add class base

---
+++ class/class-abt-base.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Admin Bar Tools base class.
+ *
+ * @author     Ken-chan
+ * @package    WordPress
+ * @subpackage Admin Bar Tools
+ * @since      1.5.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Send Chat Tools base class.
+ */
+class Abt_Base {
+
+}
