f3975fa66c54355f7e077f8d88bfd39d9c83fef8
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-17 23:15:44 +0900

    refs #86 delete comment

--- src/types/apiType.ts
+++ src/types/apiType.ts
@@ -1,13 +1,13 @@
 import { ItemsWrapperType, ItemType } from './CheckboxType';
 
 export type apiType = {
-	abt_locale?: string; // eslint-disable-line
-	abt_sc?: number; // eslint-disable-line
-	abt_status?: ItemsWrapperType< ItemType >; // eslint-disable-line
+	abt_locale?: string;
+	abt_sc?: number;
+	abt_status?: ItemsWrapperType< ItemType >;
 };
 
 export type WPApiType< T > = {
-	[ key: string ]: { // eslint-disable-line
+	[ key: string ]: {
 		[ key: string ]: T;
 	};
 };
