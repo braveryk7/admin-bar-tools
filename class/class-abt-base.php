9b438dd5d630d2347f2939e293cf47c6cd6062ee
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-17 15:26:26 +0900

    refs #83 fix var DB_VERSION -> VERSION

--- class/class-abt-base.php
+++ class/class-abt-base.php
@@ -23,7 +23,7 @@ class Abt_Base {
 	protected const PLUGIN_NAME = 'Admin Bar Tools';
 	protected const PLUGIN_FILE = self::PLUGIN_SLUG . '.php';
 	protected const TABLE_NAME  = self::PREFIX;
-	protected const DB_VERSION  = '1.4';
+	protected const VERSION     = '1.4';
 
 	private const PSI_LOCALES = [
 		'ar'    => [
