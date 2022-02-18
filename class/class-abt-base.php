269befe3b7339cbc31747be8b4b97b683d0188fa
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-18 13:43:43 +0900

    refs #79 add OPTIONS_COLUMN_NAME

--- class/class-abt-base.php
+++ class/class-abt-base.php
@@ -18,12 +18,13 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Send Chat Tools base class.
  */
 class Abt_Base {
-	protected const PREFIX      = 'abt';
-	protected const PLUGIN_SLUG = 'admin-bar-tools';
-	protected const PLUGIN_NAME = 'Admin Bar Tools';
-	protected const PLUGIN_FILE = self::PLUGIN_SLUG . '.php';
-	protected const TABLE_NAME  = self::PREFIX;
-	protected const VERSION     = '1.4';
+	protected const PREFIX              = 'abt';
+	protected const PLUGIN_SLUG         = 'admin-bar-tools';
+	protected const PLUGIN_NAME         = 'Admin Bar Tools';
+	protected const PLUGIN_FILE         = self::PLUGIN_SLUG . '.php';
+	protected const TABLE_NAME          = self::PREFIX;
+	protected const VERSION             = '1.4';
+	protected const OPTIONS_COLUMN_NAME = 'options';
 
 	public const OPTIONS_COLUMN = [
 		'options',
