b16d69451e0f8b53b7be5a921708bb110b3375a1
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-18 13:43:07 +0900

    refs #79 fix use PLUGIN_SLUG

--- class/class-abt-admin-page.php
+++ class/class-abt-admin-page.php
@@ -50,7 +50,7 @@ class Abt_Admin_Page extends Abt_Base {
 	 * @param array|string $links plugin page setting links.
 	 */
 	public function add_settings_links( array $links ): array {
-		$add_link = '<a href="options-general.php?page=admin-bar-tools">' . __( 'Settings', 'admin-bar-tools' ) . '</a>';
+		$add_link = '<a href="options-general.php?page=' . self::PLUGIN_SLUG . '">' . __( 'Settings', 'admin-bar-tools' ) . '</a>';
 		array_unshift( $links, $add_link );
 		return $links;
 	}
