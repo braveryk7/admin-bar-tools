d933dd4f35a18ffd96ae6b8a1413b9ce78aae842
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-18 16:18:28 +0900

    refs #80 add class base

---
+++ tests/class-abt-baseTest.php
@@ -0,0 +1,12 @@
+<?php
+declare( strict_types = 1 );
+
+define( 'ABSPATH', '' );
+
+require_once './class/class-abt-base.php';
+
+/**
+ * Test: Sct_Base
+ */
+class Abt_BaseTest extends PHPUnit\Framework\TestCase {
+}
