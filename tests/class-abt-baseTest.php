a2d1629d7b88fce87cd2d70fd750f99acda59589
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-18 16:27:51 +0900

    refs #80 test_get_plugin_url method

--- tests/class-abt-baseTest.php
+++ tests/class-abt-baseTest.php
@@ -30,4 +30,21 @@ class Abt_BaseTest extends PHPUnit\Framework\TestCase {
 	public function test_add_prefix() {
 		$this->assertSame( 'abt_options', $this->instance->add_prefix( 'options' ) );
 	}
+
+	/**
+	 * TEST: return_plugin_url()
+	 */
+	public function test_get_plugin_url() {
+		$method = new ReflectionMethod( $this->instance, 'get_plugin_url' );
+		$method->setAccessible( true );
+
+		$this->assertSame(
+			'https://example.com/wp-content/plugins/admin-bar-tools',
+			$method->invoke( $this->instance ),
+		);
+		$this->assertSame(
+			'https://example.com/wp-content/plugins/send-chat-tools',
+			$method->invoke( $this->instance, 'send-chat-tools' ),
+		);
+	}
 }
