803c58243247a870b7c91aa7a49485386522db70
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-25 14:18:12 +0900

    refs #116 add instance propertie

--- tests/class-abt-admin-page.php
+++ tests/class-abt-admin-page.php
@@ -5,5 +5,10 @@ declare( strict_types = 1 );
  * Test: Abt_Admin_Page
  */
 class Abt_Admin_PageTest extends PHPUnit\Framework\TestCase {
-
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
