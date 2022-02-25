36bc9108c455c9db4678bdb4f2d083b1bd9456c1
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-25 14:13:42 +0900

    refs #116 add setUpBeforeClass method

--- tests/class-abt-add-admin-barTest.php
+++ tests/class-abt-add-admin-barTest.php
@@ -11,4 +11,14 @@ class Abt_Add_Admin_Bar extends PHPUnit\Framework\TestCase {
 	 * @var object $instance instance.
 	 */
 	private $instance;
+
+	/**
+	 * Settings: ABSPATH, test class file, WordPress functions.
+	 */
+	public static function setUpBeforeClass(): void {
+		define( 'ABSPATH', '' );
+
+		require_once './class/class-abt-add-admin-bar.php';
+		require_once './tests/lib/wordpress-functions.php';
+	}
 }
