3c6b798af4e797fecd93d7c64de1507d960e0dec
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-25 14:13:12 +0900

    refs #116 add instance propertie

--- tests/class-abt-add-admin-barTest.php
+++ tests/class-abt-add-admin-barTest.php
@@ -5,4 +5,10 @@ declare( strict_types = 1 );
  * Test: Abt_Activate
  */
 class Abt_Add_Admin_Bar extends PHPUnit\Framework\TestCase {
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
