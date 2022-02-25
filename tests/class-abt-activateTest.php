dfa0a905f069c1e44928e86d5573f08bf0e84894
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-25 14:08:05 +0900

    refs #116 add instance propertie

--- tests/class-abt-activateTest.php
+++ tests/class-abt-activateTest.php
@@ -5,4 +5,10 @@ declare( strict_types = 1 );
  * Test: Abt_Activate
  */
 class Abt_ActivateTest extends PHPUnit\Framework\TestCase {
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
