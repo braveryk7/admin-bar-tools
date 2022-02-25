3031cfede10bfa668bcdb321f9a1f6344c53f057
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-25 14:15:26 +0900

    refs #116 test_add_admin_bar method

--- tests/class-abt-add-admin-barTest.php
+++ tests/class-abt-add-admin-barTest.php
@@ -29,4 +29,11 @@ class Abt_Add_Admin_BarTest extends PHPUnit\Framework\TestCase {
 	protected function setUp() :void {
 		$this->instance = new Abt_Add_Admin_Bar();
 	}
+
+	/**
+	 * TEST: add_admin_bar()
+	 */
+	public function test_add_admin_bar() {
+		$this->markTestIncomplete( 'This test is incomplete.' );
+	}
 }
