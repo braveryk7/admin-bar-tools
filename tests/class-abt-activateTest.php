2a7a584f94d7698c6653add5f70b34c38c754cad
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-25 14:08:26 +0900

    refs #116 add setUpBeforeClass method

--- tests/class-abt-activateTest.php
+++ tests/class-abt-activateTest.php
@@ -11,4 +11,14 @@ class Abt_ActivateTest extends PHPUnit\Framework\TestCase {
 	 * @var object $instance instance.
 	 */
 	private $instance;
+
+	/**
+	 * Settings: ABSPATH, test class file, WordPress functions.
+	 */
+	public static function setUpBeforeClass(): void {
+		define( 'ABSPATH', '' );
+
+		require_once './class/class-abt-activate.php';
+		require_once './tests/lib/wordpress-functions.php';
+	}
 }
