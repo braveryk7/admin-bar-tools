62c7ed68aa686a2a8cbf56e2c2e44da760c0fe0f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 00:35:31 +0900

    refs #149 fix svn ci cmd

--- .github/workflows/svn-ci.sh
+++ .github/workflows/svn-ci.sh
@@ -12,7 +12,7 @@ fi
 
 # svn info --username "${2}" "${4}"
 
-svn ci -m 'version "${1}" release' --username ${2} --password ${3}--non-interactive 
+svn ci -m 'version "${1}" release' --username ${2} --password ${3} --non-interactive 
 
 # svn ci -m "version ${1} release" --no-auth-cache --password "${3}"
 # svn up
