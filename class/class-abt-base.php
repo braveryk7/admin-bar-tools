5f66567d76e183d65c480577aaeb1678164dab3c
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-03-05 00:13:55 +0900

    refs #181 fix API_NAMESPACE -> API_NAME

--- class/class-abt-base.php
+++ class/class-abt-base.php
@@ -22,7 +22,7 @@ class Abt_Base {
 	protected const PLUGIN_SLUG         = 'admin-bar-tools';
 	protected const PLUGIN_NAME         = 'Admin Bar Tools';
 	protected const PLUGIN_FILE         = self::PLUGIN_SLUG . '.php';
-	protected const API_NAMESPACE       = self::PLUGIN_SLUG;
+	protected const API_NAME            = self::PLUGIN_SLUG;
 	protected const API_VERSION         = 'v1';
 	protected const TABLE_NAME          = self::PREFIX;
 	protected const VERSION             = '1.4';
