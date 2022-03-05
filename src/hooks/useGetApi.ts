5325ce0928a5e9e9c0b9c4c239478666eeb1e177
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-03-05 11:20:44 +0900

    refs #186 delete 2nd arg

--- src/hooks/useGetApi.ts
+++ src/hooks/useGetApi.ts
@@ -6,13 +6,13 @@ import { useEffect } from '@wordpress/element';
 import { apiType } from '../types/apiType';
 
 export const useGetApi = (
-	stateFunc: Dispatch< SetStateAction< apiType > >,
-	setApiStatus: Dispatch< SetStateAction< boolean > >
+	stateFunc: Dispatch< SetStateAction< apiType | undefined > >,
 ) => {
 	useEffect( () => {
-		apiFetch< apiType >( { path: '/admin-bar-tools/v1/options' } ).then( ( value ) => {
+		apiFetch< apiType >(
+			{ path: '/admin-bar-tools/v1/options' }
+		).then( ( value ) => {
 			stateFunc( value );
-			setApiStatus( true );
 		} );
 	}, [ stateFunc ] );
 };
