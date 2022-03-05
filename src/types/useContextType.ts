4650909ac6d2f1b4c2a28d48fed7720d8d4c690b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-03-05 11:31:13 +0900

    refs #186 fix type undefined -> null

--- src/types/useContextType.ts
+++ src/types/useContextType.ts
@@ -5,8 +5,8 @@ import { apiType } from './apiType';
 export type noticeValueType = 'abt_success' | 'abt_error' | undefined;
 
 export type apiContextType = {
-	apiData: apiType | undefined;
-	setApiData: Dispatch< SetStateAction< apiType | undefined > >;
+	apiData: apiType | null;
+	setApiData: Dispatch< SetStateAction< apiType | null > >;
 	setNoticeValue: Dispatch< SetStateAction< noticeValueType > >;
 	setNoticeMessage: Dispatch< SetStateAction< string > >;
 	snackbarTimer: number;
