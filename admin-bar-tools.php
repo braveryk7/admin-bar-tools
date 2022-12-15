b95ddca944d2832b9aa1be9a113695ce71a16fce
braveryk7 <kenta-ousaka@ciexion.com>
2022-12-15 11:39:17 +0900

    refs #233 fix use get_required_php_version()

--- admin-bar-tools.php
+++ admin-bar-tools.php
@@ -28,7 +28,7 @@ require_once dirname( __FILE__ ) . '/class/class-abt-base.php';
 require_once dirname( __FILE__ ) . '/class/class-abt-phpver-judge.php';
 
 $get_php_version_bool = new Abt_Phpver_Judge();
-if ( false === $get_php_version_bool->judgment( Abt_Base::REQUIRED_PHP_VERSION ) ) {
+if ( false === $get_php_version_bool->judgment( Abt_Base::get_required_php_version() ) ) {
 	require_once ABSPATH . 'wp-admin/includes/plugin.php';
 	if ( is_plugin_active( plugin_basename( __FILE__ ) ) ) {
 		if ( is_admin() ) {
@@ -40,7 +40,7 @@ if ( false === $get_php_version_bool->judgment( Abt_Base::REQUIRED_PHP_VERSION )
 		echo '<p>' . esc_html_e( 'Admin Bar Tools requires at least PHP 7.3.0 or later.', 'admin-bar-tools' ) . esc_html_e( 'Please upgrade PHP.', 'admin-bar-tools' ) . '</p>';
 		exit;
 	}
-} elseif ( true === $get_php_version_bool->judgment( Abt_Base::REQUIRED_PHP_VERSION ) ) {
+} elseif ( true === $get_php_version_bool->judgment( Abt_Base::get_required_php_version() ) ) {
 	require_once dirname( __FILE__ ) . '/class/class-abt-activate.php';
 	require_once dirname( __FILE__ ) . '/class/class-abt-admin-page.php';
 	require_once dirname( __FILE__ ) . '/class/class-abt-add-admin-bar.php';
