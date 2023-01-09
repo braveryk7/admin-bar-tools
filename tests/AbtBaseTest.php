956b9698375742f8a033adbbae0c791c15b0e77e
braveryk7 <kenta-ousaka@ciexion.com>
2023-01-09 23:25:24 +0900

    refs #246 fix move test_get_plugin_name method

--- tests/AbtBaseTest.php
+++ tests/AbtBaseTest.php
@@ -40,13 +40,6 @@ class AbtBaseTest extends PHPUnit\Framework\TestCase {
 	}
 
 	/**
-	 * TEST: get_plugin_name()
-	 */
-	public function test_get_plugin_name() {
-		$this->assertSame( 'Admin Bar Tools', $this->instance->get_plugin_name() );
-	}
-
-	/**
 	 * TEST: return_plugin_url()
 	 */
 	public function test_get_plugin_url() {
@@ -61,6 +54,13 @@ class AbtBaseTest extends PHPUnit\Framework\TestCase {
 			'https://example.com/wp-content/plugins/send-chat-tools',
 			$method->invoke( $this->instance, 'send-chat-tools' ),
 		);
+	}
+
+	/**
+	 * TEST: get_plugin_name()
+	 */
+	public function test_get_plugin_name() {
+		$this->assertSame( 'Admin Bar Tools', $this->instance->get_plugin_name() );
 	}
 
 	/**
