84564fb8422e1a89754f1fe32f36600d11ae9659
braveryk7 <kenta-ousaka@ciexion.com>
2023-03-31 17:11:40 +0900

    refs #325 fix use locales -> common/locales.json

--- src/components/atoms/Select.tsx
+++ src/components/atoms/Select.tsx
@@ -3,20 +3,19 @@ import { memo, useContext } from '@wordpress/element';
 
 import { useChangeValue } from 'src/hooks/useChangeValue';
 import { apiContext } from 'src/index';
-import { psiLocales } from 'src/utils/constant';
 
 import { itemKeyType } from 'src/types/apiType';
 
-export const Select = memo( ( props: { itemKey: itemKeyType } ) => {
+export const Select = memo( ( props: { itemKey: itemKeyType, locales?: object } ) => {
 	const { apiData } = useContext( apiContext );
-	const { itemKey } = props;
+	const { itemKey, locales } = props;
 	const changeValue = useChangeValue( itemKey );
 
 	return (
 		<>
-			{ apiData && <SelectControl
+			{ apiData && locales && <SelectControl
 				value={ apiData.locale }
-				options={ Object.values( psiLocales ).map( ( locale ) => ( {
+				options={ Object.values( locales ).map( ( locale ) => ( {
 					label: locale.name,
 					value: locale.id,
 				} ) ) }
