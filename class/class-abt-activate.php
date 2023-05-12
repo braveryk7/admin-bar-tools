8a031141592814e4f412a01d46f3f2234122b5e3
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-05-12 11:00:37 +0900

    refs #341 fix use get_version method

--- class/class-abt-activate.php
+++ class/class-abt-activate.php
@@ -52,7 +52,7 @@ class Abt_Activate extends Abt_Base {
 			$this->register_options();
 		}
 
-		if ( self::VERSION !== $abt_options['version'] ) {
+		if ( $this->get_version() !== $abt_options['version'] ) {
 			foreach ( self::OPTIONS_KEY as $key_name ) {
 				if ( ! array_key_exists( $key_name, $abt_options ) ) {
 					if ( 'theme_support' === $key_name ) {
