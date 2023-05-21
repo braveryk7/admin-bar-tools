e5a467f285787e18293a4418a1e4e6232532dd96
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-05-22 05:35:19 +0900

    refs #362 initial commit

---
+++ tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Never_Let_Me_Go
+ */
+
+use PHP_CodeSniffer\Tokenizers\PHP;
+
+require dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+// Start up the WP testing environment.
+require_once $_tests_dir . '/includes/bootstrap.php';
