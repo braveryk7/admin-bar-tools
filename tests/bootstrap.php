4f441acadf5de439017f5abe00167a4fafc7fe9a
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-05-22 06:09:49 +0900

    refs #362 fix require autoloader line

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
-require_once dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
-
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
 	exit( PHP_EOL . "\033[41mWP_TESTS_DIR environment variable is not defined.\033[0m" . PHP_EOL . PHP_EOL );
 }
+
+require_once dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
 require_once $_tests_dir . '/includes/bootstrap.php';
