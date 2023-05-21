f49702b42b7e5240c8a7511fefc8aa111c5c0157
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-05-22 06:12:26 +0900

    refs #362 add ROOT_DIR

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@ if ( ! $_tests_dir ) {
 	exit( PHP_EOL . "\033[41mWP_TESTS_DIR environment variable is not defined.\033[0m" . PHP_EOL . PHP_EOL );
 }
 
+define( 'ROOT_DIR', dirname( dirname( __FILE__ ) ) );
+
 require_once dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
 require_once $_tests_dir . '/includes/bootstrap.php';
