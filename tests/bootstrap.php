954f0b0a0c7c1a1322472035aa854c48da947321
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-05-22 05:57:56 +0900

    refs #362 remove unused require

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,4 @@ if ( ! $_tests_dir ) {
 	exit( PHP_EOL . "\033[41mWP_TESTS_DIR environment variable is not defined.\033[0m" . PHP_EOL . PHP_EOL );
 }
 
-require_once $_tests_dir . '/includes/functions.php';
-
 require_once $_tests_dir . '/includes/bootstrap.php';
