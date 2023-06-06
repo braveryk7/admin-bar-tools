949394996d0c405360327f822b834222855104f3
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-06 12:41:25 +0900

    refs #389 initial commit

---
+++ conf/stubs/admin-bar-tools-tests-stubs.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Admin Bar Tools stub for PHPStan.
+ *
+ * @author     Ken-chan
+ * @package    WordPress
+ * @subpackage Admin Bar Tools
+ * @since      1.7.0
+ */
+
+// Constants defined in tests/bootstrap.php.
+define( 'ROOT_DIR', '' );
