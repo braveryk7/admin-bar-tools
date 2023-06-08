156957479a3a8baadc67e2bb8060cf20cec220ef
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-08 16:24:16 +0900

    refs #420 add instance property

--- tests/class-abt-phpver-judge-test.php
+++ tests/class-abt-phpver-judge-test.php
@@ -7,4 +7,10 @@ use Yoast\WPTestUtils\WPIntegration\TestCase;
  * Test: Abt_Activate
  */
 class Abt_Phpver_Judge_Test extends TestCase {
+	/**
+	 * This test class instance.
+	 *
+	 * @var Abt_Add_Admin_Bar $instance instance.
+	 */
+	private $instance;
 }
