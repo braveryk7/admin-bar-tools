cea3236f65b0562e51e554b7b40bb99b5ec1cbd5
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-08 16:26:30 +0900

    refs #420 add set_up_before_class method

--- tests/class-abt-phpver-judge-test.php
+++ tests/class-abt-phpver-judge-test.php
@@ -13,4 +13,11 @@ class Abt_Phpver_Judge_Test extends TestCase {
 	 * @var Abt_Add_Admin_Bar $instance instance.
 	 */
 	private $instance;
+
+	/**
+	 * Settings: ABSPATH, test class file, WordPress functions.
+	 */
+	public static function set_up_before_class(): void {
+		require_once ROOT_DIR . '/class/class-abt-phpver-judge.php';
+	}
 }
