38a1369278747efafd779ac466b63c267b92a3c3
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-09 16:41:11 +0900

    refs #425 add use is_string() method

--- class/class-abt-add-admin-bar.php
+++ class/class-abt-add-admin-bar.php
@@ -36,11 +36,16 @@ class Abt_Add_Admin_Bar extends Abt_Base {
 			return;
 		}
 
-		$query           = intval( get_query_var( 'paged' ) );
-		$url             = rawurlencode( get_pagenum_link( $query ) );
-		$add_url_lists   = [ 'psi', 'lh', 'gc', 'gi', 'bi', 'twitter', 'facebook' ];
-		$sanitize_domain = isset( $_SERVER['HTTP_HOST'] ) ? sanitize_text_field( wp_unslash( $_SERVER['HTTP_HOST'] ) ) : '';
-		$sanitize_uri    = isset( $_SERVER['REQUEST_URI'] ) ? sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) ) : '';
+		$query         = intval( get_query_var( 'paged' ) );
+		$url           = rawurlencode( get_pagenum_link( $query ) );
+		$add_url_lists = [ 'psi', 'lh', 'gc', 'gi', 'bi', 'twitter', 'facebook' ];
+
+		$sanitize_domain = isset( $_SERVER['HTTP_HOST'] ) && is_string( $_SERVER['HTTP_HOST'] )
+			? sanitize_text_field( wp_unslash( $_SERVER['HTTP_HOST'] ) )
+			: '';
+		$sanitize_uri    = isset( $_SERVER['REQUEST_URI'] ) && is_string( $_SERVER['REQUEST_URI'] )
+			? sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) )
+			: '';
 
 		if ( is_user_logged_in() ) {
 			$wp_admin_bar->add_node(
