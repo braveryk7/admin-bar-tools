9ab4025cf96c4c776454e220307cf1cb2149255f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-12 18:25:23 +0900

    refs #429 add set_up method

--- tests/class-abt-options.php
+++ tests/class-abt-options.php
@@ -12,4 +12,13 @@ class Abt_Options_Test extends TestCase {
 	 * @var Abt_Options $instance instance.
 	 */
 	private $instance;
+
+	/**
+	 * SetUp.
+	 * Create instance.
+	 */
+	public function set_up() :void {
+		parent::set_up();
+		$this->instance = Abt_Options::get_instance();
+	}
 }
