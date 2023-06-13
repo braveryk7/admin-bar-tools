c51eae8cc919e93cb69d5535069e509f25d292cd
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-13 21:14:28 +0900

    refs #430 add abt_options init

--- class/class-abt-activate.php
+++ class/class-abt-activate.php
@@ -27,8 +27,12 @@ class Abt_Activate extends Abt_Base {
 
 	/**
 	 * Constructor.
+	 *
+	 * @param Abt_Options $abt_options Abt_Options instance.
 	 */
-	public function __construct() {
+	public function __construct( Abt_Options $abt_options ) {
+		$this->abt_options = $abt_options;
+
 		register_activation_hook( $this->get_plugin_path(), [ $this, 'register_options' ] );
 		add_action( 'init', [ $this, 'update_abt_options' ], 10 );
 		add_filter( 'http_request_args', [ $this, 'check_environment' ], 0, 1 );
