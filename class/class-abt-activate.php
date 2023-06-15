85456487181d1de9cc667b8d630e08767a967abe
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-15 21:58:27 +0900

    refs #430 fix use Abt_Options

--- class/class-abt-activate.php
+++ class/class-abt-activate.php
@@ -35,7 +35,7 @@ class Abt_Activate extends Abt_Base {
 	public function __construct( Abt_Options $abt_options ) {
 		$this->abt_options = $abt_options;
 
-		register_activation_hook( $this->get_plugin_path(), [ $this, 'register_options' ] );
+		register_activation_hook( $this->get_plugin_path(), [ $abt_options, 'register_options' ] );
 		add_filter( 'http_request_args', [ $this, 'check_environment' ], 0, 1 );
 	}
 
