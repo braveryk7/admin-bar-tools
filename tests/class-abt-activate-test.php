7b81a5f3ca56d2f424ae8b09e1b54cab53ca3574
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-16 14:07:45 +0900

    refs #430 remove unused method

--- tests/class-abt-activate-test.php
+++ tests/class-abt-activate-test.php
@@ -60,50 +60,6 @@ class Abt_Activate_Test extends TestCase {
 	}
 
 	/**
-	 * TEST: update_abt_options
-	 */
-	public function test_update_abt_options(): void {
-		$abt_base                 = new Abt_Base();
-		$abt_base_get_abt_options = new ReflectionMethod( $abt_base, 'get_abt_options' );
-		$abt_base_get_abt_options->setAccessible( true );
-
-		$get_abt_options = function() use ( $abt_base_get_abt_options ) {
-			return $abt_base_get_abt_options->invoke( $this->instance );
-		};
-
-		$delete_abt_options = function() {
-			delete_option( 'abt_options' );
-		};
-
-		if ( $get_abt_options() ) {
-			$delete_abt_options();
-		}
-
-		$this->assertTrue( empty( $get_abt_options() ) );
-
-		$this->instance->update_abt_options();
-
-		$this->assertTrue( ! empty( $get_abt_options() ) );
-
-		$abt_options = $get_abt_options();
-
-		$this->assertIsArray( $abt_options );
-
-		$abt_options['version'] = '0.0.0';
-		unset( $abt_options['theme_support'] );
-
-		$this->assertArrayNotHasKey( 'theme_support', $abt_options );
-
-		$this->instance->update_abt_options();
-
-		$actual_abt_options = $get_abt_options();
-		$this->assertIsArray( $actual_abt_options );
-
-		$this->assertNotSame( $abt_options['version'], $actual_abt_options['version'] );
-		$this->assertArrayHasKey( 'theme_support', $actual_abt_options );
-	}
-
-	/**
 	 * TEST: register_options()
 	 *
 	 * @testWith [ "items", null ]
